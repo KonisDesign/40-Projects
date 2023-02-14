eb0095a2ddce1d5e1d407192012d2add7e66d6c4
KonisDesign <lhacktu@gmail.com>
2023-02-14 12:20:04 +0100

    resume added

--- 13. Portfolio Website/script.js
+++ 13. Portfolio Website/script.js
@@ -1,8 +1,10 @@
 function show(id, button) {
     document.getElementById('home').style.display = "none";
     document.getElementById('about').style.display = "none";
+    document.getElementById('resume').style.display = "none";
     document.getElementById('homebutton').classList.remove('active');
     document.getElementById('aboutbutton').classList.remove('active');
+    document.getElementById('resumebutton').classList.remove('active');
 
 
     document.getElementById(id).style.display = "flex";
