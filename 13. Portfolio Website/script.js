9ce1968cc73835a7960ee966b4e490852b9fa147
KonisDesign <lhacktu@gmail.com>
2023-02-14 14:02:48 +0100

    All pages done

--- 13. Portfolio Website/script.js
+++ 13. Portfolio Website/script.js
@@ -2,9 +2,11 @@ function show(id, button) {
     document.getElementById('home').style.display = "none";
     document.getElementById('about').style.display = "none";
     document.getElementById('resume').style.display = "none";
+    document.getElementById('projects').style.display = "none";
     document.getElementById('homebutton').classList.remove('active');
     document.getElementById('aboutbutton').classList.remove('active');
     document.getElementById('resumebutton').classList.remove('active');
+    document.getElementById('projectbutton').classList.remove('active');
 
 
     document.getElementById(id).style.display = "flex";
