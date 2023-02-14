0bd8aee534ec6fa3763dc2408c651b2f97da2036
KonisDesign <lhacktu@gmail.com>
2023-02-14 09:26:28 +0100

    add js navigation

---
+++ 13. Portfolio Website/script.js
@@ -0,0 +1,10 @@
+function show(id, button) {
+    document.getElementById('home').style.display = "none";
+    document.getElementById('about').style.display = "none";
+    document.getElementById('homebutton').classList.remove('active');
+    document.getElementById('aboutbutton').classList.remove('active');
+
+
+    document.getElementById(id).style.display = "flex";
+    document.getElementById(button).classList.add("active");
+}
