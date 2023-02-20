4b13d6dcb6544118a5f92245fad5b7a45bb28dce
KonisDesign <lhacktu@gmail.com>
2023-02-20 11:55:09 +0100

    Fix increment value on a same object

--- 8. Ecommerce Website/cart.js
+++ 8. Ecommerce Website/cart.js
@@ -1,28 +1,37 @@
 let cart = [];
 let quantity = [];
+let totalPrice = 0;
 
-if (localStorage.getItem('cart') != null) {
+if (localStorage.getItem('cart') != null && JSON.parse(localStorage.getItem('cart')).length != 0) {
     cart = JSON.parse(localStorage.getItem('cart'));
     quantity = JSON.parse(localStorage.getItem('quantity'));
-}
-
-fetch('data.json')
+    console.log(cart);
+    fetch('data.json')
     .then(response => response.json())
     .then(data => {
         for (let i = 0; i < cart.length; i++) {
             document.getElementById('purchases-container').innerHTML += '<div class="row"><img class="small" src="assets/' + data[cart[i]].collectionid + '/' + data[cart[i]].nameid + '-1.png" alt="item"><h4 class="big">' + data[cart[i]].name + '</h4><p class="big">' + data[cart[i]].price + '€</p><input class="normal" type="number" id="number' + i + '" onchange="changeQuantity(' + i + ')" value="' + quantity[i] + '"><p class="normal"">' + Number(data[cart[i]].price) * Number(quantity[i]) + '€</p><button class="small" onclick="removeItem(' + i + ')">X</button></div>';
+            totalPrice += (Number(data[cart[i]].price) * Number(quantity[i]));
+            document.getElementById('subtotal').innerHTML = totalPrice + '€';
+            document.getElementById('total').innerHTML = totalPrice + '€';
         }
     })
     .catch(error => {
         console.error('Error:', error);
     });
+} else {
+    document.getElementById('purchases-container').innerHTML = '<div class="nopurchase"><h1>Your cart is empty</h1></div>';
+    document.getElementById('titleList').style.display = 'none';
+    document.getElementById('clearCart').style.display = 'none';
+    document.getElementById('checkout').style.display = 'none';
+}
+
+
 
 function changeQuantity(id) {
     quantity.splice(id, 1, document.getElementById('number' + id).value);
     localStorage.setItem('quantity', JSON.stringify(quantity));
     window.location.reload();
-    console.log(cart);
-    console.log(quantity);
 }
 
 function removeItem(id) {
@@ -31,8 +40,6 @@ function removeItem(id) {
     localStorage.setItem('cart', JSON.stringify(cart));
     localStorage.setItem('quantity', JSON.stringify(quantity));
     window.location.reload();
-    console.log(cart);
-    console.log(quantity);
 }
 
 function clearCart() {
--- 8. Ecommerce Website/product.js
+++ 8. Ecommerce Website/product.js
@@ -10,15 +10,15 @@ function readData() {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
-                for (let i = 0; i < data.length; i++) {
-                    if (urlParams.has('product') == true) {
-                        document.getElementById('product-container').innerHTML = '<div class="product-container"><div class="snapshot-container"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png" alt="snap1"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-2.avif" alt="snap2"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-3.avif" alt="snap3"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-4.avif" alt="snap"></div><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png"><div class="options-container"><h2>' + data[urlParams.get('product')].name + '</h2><h1>' + data[urlParams.get('product')].price + '€</h1><p>' + data[urlParams.get('product')].description + '</p><div class="actions-container"><input type="number" id="quantityinput" value="1" min="1" max="10"><button class="primary-button" onclick="addToCart(\'' + data[urlParams.get('product')].id + '\')">Add to cart</button></div><div class="row-container"><h3>SKU:</h3><p>' + data[urlParams.get('product')].id + '</p></div><div class="row-container"><p>Share this items</p><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-facebook-f"></i></a><a href="#"><i class="fa-brands fa-pinterest-p"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a></div></div></div><div class="description-container"><h2>Description</h2><div class="detail-container"><div class="text-container"><h1>Detail</h1><p>' + data[urlParams.get('product')].description + '</p></div><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png" alt="detail"></div></div>';
-                        break;
-                    } else {
-                        window.location.href = "./";
-                        break;
-                    }
+            for (let i = 0; i < data.length; i++) {
+                if (urlParams.has('product') == true) {
+                    document.getElementById('product-container').innerHTML = '<div class="product-container"><div class="snapshot-container"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png" alt="snap1"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-2.avif" alt="snap2"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-3.avif" alt="snap3"><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-4.avif" alt="snap"></div><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png"><div class="options-container"><h2>' + data[urlParams.get('product')].name + '</h2><h1>' + data[urlParams.get('product')].price + '€</h1><p>' + data[urlParams.get('product')].description + '</p><div class="actions-container"><input type="number" id="quantityinput" value="1" min="1" max="10"><button class="primary-button" onclick="addToCart(\'' + data[urlParams.get('product')].id + '\')">Add to cart</button></div><div class="row-container"><h3>SKU:</h3><p>' + data[urlParams.get('product')].id + '</p></div><div class="row-container"><p>Share this items</p><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-facebook-f"></i></a><a href="#"><i class="fa-brands fa-pinterest-p"></i></a><a href="#"><i class="fa-brands fa-instagram"></i></a></div></div></div><div class="description-container"><h2>Description</h2><div class="detail-container"><div class="text-container"><h1>Detail</h1><p>' + data[urlParams.get('product')].description + '</p></div><img src="assets/' + data[urlParams.get('product')].collectionid + '/' + data[urlParams.get('product')].nameid + '-1.png" alt="detail"></div></div>';
+                    break;
+                } else {
+                    window.location.href = "./";
+                    break;
                 }
+            }
         })
         .catch(error => {
             console.error('Error:', error);
@@ -26,14 +26,30 @@ function readData() {
 }
 
 function addToCart(id) {
-    console.log("fdgd")
-    if (localStorage.getItem('cart') != null) {
+        console.log('test')
         cart = JSON.parse(localStorage.getItem('cart'));
         quantity = JSON.parse(localStorage.getItem('quantity'));
-    }
+        if (cart == null) {
+            cart = [];
+            quantity = [];
+        }
+        if (cart.length > 0) {
+            for (let i = 0; i < cart.length; i++) {
+                if (id == cart[i]) {
+                    let newquantity = Number(quantity[i]) + Number(document.getElementById('quantityinput').value);
+                    quantity.splice(i, 1, newquantity);
+                } else if (i == (cart.length - 1)) {
+                    cart.push(id);
+                    quantity.push(document.getElementById('quantityinput').value);
+                    break
+                }
+            }
+        } else {
+            cart.push(id);
+            quantity.push(document.getElementById('quantityinput').value);
+        }
 
-    cart.push(id);
-    quantity.push(document.getElementById('quantityinput').value);
+
     localStorage.setItem('cart', JSON.stringify(cart));
     localStorage.setItem('quantity', JSON.stringify(quantity));
 }
