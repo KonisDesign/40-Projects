875c79ca162b6088bddc6dbc69e63fb162e54d7e
KonisDesign <lhacktu@gmail.com>
2023-02-23 12:22:50 +0100

    final

--- 11. Tic Tac Toe/script.js
+++ 11. Tic Tac Toe/script.js
@@ -1,5 +1,13 @@
 let game = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 let myturn = 1;
+let level = 0;
+
+function startGame(param) {
+    level = Number(param);
+    document.getElementById("game").style.display = "flex";
+    document.getElementById("title").classList.add("tracking-out-expand-fwd-top");
+    document.getElementById("level").classList.add("tracking-out-expand-fwd-top");
+}
 
 function press(where, buttonid, team) {
     game.splice(where, 1, team);
@@ -28,104 +36,107 @@ function detecWin() {
     }
 }
 
-
 function iaPlay() {
     if (detectEmpty()) {
-        if (game[0] == "O" && game[0] == game[1] && game[2] != "X" && game[2] != "O") {
-            press(2, "button2", "O");
-        } else if (game[2] == "O" && game[2] == game[1] && game[0] != "X" && game[0] != "O") {
+        if (game[4] == "X" && game[0] == "0" && game[2] == "2" && game[6] == "6" && game[8] == "8" && level == 2) {
             press(0, "button0", "O");
-        } else if (game[0] == "O" && game[0] == game[2] && game[1] != "X" && game[1] != "O") {
+        } else if ((game[0] == "X" || game[2] == "X" || game[6] == "X" || game[8] == "X") && game[4] == "4" && level == 2) {
+            press(4, "button4", "O");
+        } else if (game[0] == "O" && game[0] == game[1] && game[2] == "2" && level > 0) {
+            press(2, "button2", "O");
+        } else if (game[2] == "O" && game[2] == game[1] && game[0] == "0" && level > 0) {
+            press(0, "button0", "O");
+        } else if (game[0] == "O" && game[0] == game[2] && game[1] == "1" && level > 0) {
             press(1, "button1", "O");
-        } else if (game[3] == "O" && game[3] == game[4] && game[5] != "X" && game[5] != "O") {
+        } else if (game[3] == "O" && game[3] == game[4] && game[5] == "5" && level > 0) {
             press(5, "button5", "O");
-        } else if (game[5] == "O" && game[5] == game[4] && game[3] != "X" && game[3] != "O") {
+        } else if (game[5] == "O" && game[5] == game[4] && game[3] == "3" && level > 0) {
             press(3, "button3", "O");
-        } else if (game[3] == "O" && game[3] == game[5] && game[4] != "X" && game[4] != "O") {
+        } else if (game[3] == "O" && game[3] == game[5] && game[4] == "4" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[6] == "O" && game[6] == game[7] && game[8] != "X" && game[8] != "O") {
+        } else if (game[6] == "O" && game[6] == game[7] && game[8] == "8" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == "O" && game[8] == game[7] && game[6] != "X" && game[6] != "O") {
+        } else if (game[8] == "O" && game[8] == game[7] && game[6] == "6" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == "O" && game[6] == game[8] && game[7] != "X" && game[7] != "O") {
+        } else if (game[6] == "O" && game[6] == game[8] && game[7] == "7" && level > 0) {
             press(7, "button7", "O");
-        } else if (game[0] == "O" && game[0] == game[3] && game[6] != "X" && game[6] != "O") {
+        } else if (game[0] == "O" && game[0] == game[3] && game[6] == "6" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == "O" && game[6] == game[3] && game[0] != "X" && game[0] != "O") {
+        } else if (game[6] == "O" && game[6] == game[3] && game[0] == "0" && level > 0) {
             press(0, "button0", "O");
-        } else if (game[0] == "O" && game[0] == game[6] && game[3] != "X" && game[3] != "O") {
+        } else if (game[0] == "O" && game[0] == game[6] && game[3] == "3" && level > 0) {
             press(3, "button3", "O");
-        } else if (game[1] == "O" && game[1] == game[4] && game[7] != "X" && game[7] != "O") {
+        } else if (game[1] == "O" && game[1] == game[4] && game[7] == "7" && level > 0) {
             press(7, "button7", "O");
-        } else if (game[7] == "O" && game[7] == game[4] && game[1] != "X" && game[1] != "O") {
+        } else if (game[7] == "O" && game[7] == game[4] && game[1] == "1" && level > 0) {
             press(1, "button1", "O");
-        } else if (game[1] == "O" && game[1] == game[7] && game[4] != "X" && game[4] != "O") {
+        } else if (game[1] == "O" && game[1] == game[7] && game[4] == "4" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[2] == "O" && game[2] == game[5] && game[8] != "X" && game[8] != "O") {
+        } else if (game[2] == "O" && game[2] == game[5] && game[8] == "8" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == "O" && game[8] == game[5] && game[2] != "X" && game[2] != "O") {
+        } else if (game[8] == "O" && game[8] == game[5] && game[2] == "2" && level > 0) {
             press(2, "button2", "O");
-        } else if (game[2] == "O" && game[2] == game[8] && game[5] != "X" && game[5] != "O") {
+        } else if (game[2] == "O" && game[2] == game[8] && game[5] == "5" && level > 0) {
             press(5, "button5", "O");
-        } else if (game[0] == "O" && game[0] == game[4] && game[8] != "X" && game[8] != "O") {
+        } else if (game[0] == "O" && game[0] == game[4] && game[8] == "8" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == "O" && game[8] == game[4] && game[0] != "X" && game[0] != "O") {
+        } else if (game[8] == "O" && game[8] == game[4] && game[0] == "0" && level > 0) {
             press(0, "button0", "O");
-        } else if (game[0] == "O" && game[0] == game[8] && game[4] != "X" && game[4] != "O") {
+        } else if (game[0] == "O" && game[0] == game[8] && game[4] == "4" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[2] == "O" && game[2] == game[4] && game[6] != "X" && game[6] != "O") {
+        } else if (game[2] == "O" && game[2] == game[4] && game[6] == "6" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == "O" && game[6] == game[4] && game[2] != "X" && game[2] != "O") {
+        } else if (game[6] == "O" && game[6] == game[4] && game[2] == "2" && level > 0) {
             press(2, "button2", "O");
-        } else if (game[2] == "O" && game[2] == game[6] && game[4] != "X" && game[4] != "O") {
+        } else if (game[2] == "O" && game[2] == game[6] && game[4] == "4" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[0] == game[1] && game[2] != "X" && game[2] != "O") {
+        } else if (game[0] == game[1] && game[2] != "X" && game[2] != "O" && level > 0) {
             press(2, "button2", "O");
-        } else if (game[2] == game[1] && game[0] != "X" && game[0] != "O") {
+        } else if (game[2] == game[1] && game[0] != "X" && game[0] != "O" && level > 0) {
             press(0, "button0", "O");
-        } else if (game[0] == game[2] && game[1] != "X" && game[1] != "O") {
+        } else if (game[0] == game[2] && game[1] != "X" && game[1] != "O" && level > 0) {
             press(1, "button1", "O");
-        } else if (game[3] == game[4] && game[5] != "X" && game[5] != "O") {
+        } else if (game[3] == game[4] && game[5] != "X" && game[5] != "O" && level > 0) {
             press(5, "button5", "O");
-        } else if (game[5] == game[4] && game[3] != "X" && game[3] != "O") {
+        } else if (game[5] == game[4] && game[3] != "X" && game[3] != "O" && level > 0) {
             press(3, "button3", "O");
-        } else if (game[3] == game[5] && game[4] != "X" && game[4] != "O") {
+        } else if (game[3] == game[5] && game[4] != "X" && game[4] != "O" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[6] == game[7] && game[8] != "X" && game[8] != "O") {
+        } else if (game[6] == game[7] && game[8] != "X" && game[8] != "O" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == game[7] && game[6] != "X" && game[6] != "O") {
+        } else if (game[8] == game[7] && game[6] != "X" && game[6] != "O" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == game[8] && game[7] != "X" && game[7] != "O") {
+        } else if (game[6] == game[8] && game[7] != "X" && game[7] != "O" && level > 0) {
             press(7, "button7", "O");
-        } else if (game[0] == game[3] && game[6] != "X" && game[6] != "O") {
+        } else if (game[0] == game[3] && game[6] != "X" && game[6] != "O" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == game[3] && game[0] != "X" && game[0] != "O") {
+        } else if (game[6] == game[3] && game[0] != "X" && game[0] != "O" && level > 0) {
             press(0, "button0", "O");
-        } else if (game[0] == game[6] && game[3] != "X" && game[3] != "O") {
+        } else if (game[0] == game[6] && game[3] != "X" && game[3] != "O" && level > 0) {
             press(3, "button3", "O");
-        } else if (game[1] == game[4] && game[7] != "X" && game[7] != "O") {
+        } else if (game[1] == game[4] && game[7] != "X" && game[7] != "O" && level > 0) {
             press(7, "button7", "O");
-        } else if (game[7] == game[4] && game[1] != "X" && game[1] != "O") {
+        } else if (game[7] == game[4] && game[1] != "X" && game[1] != "O" && level > 0) {
             press(1, "button1", "O");
-        } else if (game[1] == game[7] && game[4] != "X" && game[4] != "O") {
+        } else if (game[1] == game[7] && game[4] != "X" && game[4] != "O" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[2] == game[5] && game[8] != "X" && game[8] != "O") {
+        } else if (game[2] == game[5] && game[8] != "X" && game[8] != "O" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == game[5] && game[2] != "X" && game[2] != "O") {
+        } else if (game[8] == game[5] && game[2] != "X" && game[2] != "O" && level > 0) {
             press(2, "button2", "O");
-        } else if (game[2] == game[8] && game[5] != "X" && game[5] != "O") {
+        } else if (game[2] == game[8] && game[5] != "X" && game[5] != "O" && level > 0) {
             press(5, "button5", "O");
-        } else if (game[0] == game[4] && game[8] != "X" && game[8] != "O") {
+        } else if (game[0] == game[4] && game[8] != "X" && game[8] != "O" && level > 0) {
             press(8, "button8", "O");
-        } else if (game[8] == game[4] && game[0] != "X" && game[0] != "O") {
+        } else if (game[8] == game[4] && game[0] != "X" && game[0] != "O" && level > 0) {
             press(0, "button0", "O");
-        } else if (game[0] == game[8] && game[4] != "X" && game[4] != "O") {
+        } else if (game[0] == game[8] && game[4] != "X" && game[4] != "O" && level > 0) {
             press(4, "button4", "O");
-        } else if (game[2] == game[4] && game[6] != "X" && game[6] != "O") {
+        } else if (game[2] == game[4] && game[6] != "X" && game[6] != "O" && level > 0) {
             press(6, "button6", "O");
-        } else if (game[6] == game[4] && game[2] != "X" && game[2] != "O") {
+        } else if (game[6] == game[4] && game[2] != "X" && game[2] != "O" && level > 0) {
             press(2, "button2", "O");
-        } else if (game[2] == game[6] && game[4] != "X" && game[4] != "O") {
+        } else if (game[2] == game[6] && game[4] != "X" && game[4] != "O" && level > 0) {
             press(4, "button4", "O");
         } else {
             let i = Math.floor(Math.random() * 9);
@@ -157,16 +168,11 @@ function whoWin(id1, id2, id3) {
         document.getElementById('button' + i).disabled = true;
     }
     if (game[id1] == "X") {
-        document.getElementById('button' + id1).style.background = "green";
-        document.getElementById('button' + id2).style.background = "green";
-        document.getElementById('button' + id3).style.background = "green";
         document.getElementById("winner").innerHTML = "You win !";
     } else if (game[id1] == "O") {
-        document.getElementById('button' + id1).style.background = "red";
-        document.getElementById('button' + id2).style.background = "red";
-        document.getElementById('button' + id3).style.background = "red";
         document.getElementById("winner").innerHTML = "You loose !";
     }
+    
 
     document.getElementById("newgame").style.display = "block";
 }
